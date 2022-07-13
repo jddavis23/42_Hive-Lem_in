a6fd0fe8744df10c74fdf61549afee0a2508fa64
Jefferson Davis <davisjd.jefferson23@gmail.com>
2022-07-13 13:31:35 +0300

    builing function that will collect the maximal paths that do not clash, try experimenting with passing in the address of the indicies that need to be iterated through as not to change their values (may not work). Also using the connections of start and finish to calculate the maximal amount of paths will not work. Look at example in book

---
+++ lemin/final_paths.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   final_paths.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jdavis <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/13 13:00:01 by jdavis            #+#    #+#             */
+/*   Updated: 2022/07/13 13:27:22 by jdavis           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	absolute_path(t_room * pass)
+{
+	t_index	*tmp_indx;
+	t_path 	*tmp_path;
+	int		j;
+
+	j = 1;
+	while ( j <= length of longest path)
+	{
+		while (pass->path)
+		{
+			if (pass->path->len == j)
+			{
+				if (!pass->path->next || path->next->len > j)
+					//keep the path we're currently on
+					//break loop
+				tmp_indx = path->move;
+				tmp_path = path;
+				tmp_path = tmp_path->next;
+				while (tmp_path->len == j)
+				{
+					if (tmp_path->move->index == tmp_index->index)
+					{
+						find_diff(t_ //experiement with functions only person in elements and see if their values change. In theory, values should not because shifting value of address passed in and not what is containing that address.					}
+				}
+			}
+		}
+	}
+}
