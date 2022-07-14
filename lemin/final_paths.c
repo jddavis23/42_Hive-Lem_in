a17554caee14ebb4967bcf517b63838b67a4451f
Jefferson Davis <davisjd.jefferson23@gmail.com>
2022-07-14 17:40:05 +0300

    final_path.c currently aiming to delete replicating index's in longer length paths (might be problem with this method; might not end up using the compared paths, so potentially eliminating pths that can be used

--- lemin/final_paths.c
+++ lemin/final_paths.c
@@ -6,38 +6,111 @@
 /*   By: jdavis <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/13 13:00:01 by jdavis            #+#    #+#             */
-/*   Updated: 2022/07/14 15:51:08 by jdavis           ###   ########.fr       */
+/*   Updated: 2022/07/14 17:36:09 by jdavis           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+int	compare_paths(t_index *first, t_index *second)
+{
+	t_index *head_first;
+	t_index *head_second;
+	int		sim;
+
+	head_second = second;
+	head_first = first;
+	sim = -1;
+	while (first)
+	{
+		while (second)
+		{
+			if (first->index == second->index)
+			{
+				sim = first->index;
+				break ;
+			}
+			second = second->next;
+		}
+		if (sim != -1)
+			break ;
+		first = first->next;
+	}
+	if (sim == -1)
+	{
+		//return a value to indicate keep both
+	}
+
+
+}
+
+int	keep_and_erase(int p_nbr, t_path *path, t_path *head)
+{
+	t_path *temp;
+	int		sim;
+
+	temp = path;
+	while (head && (head->len != path->len || head->nbr != p_nbr))
+		head = head->next;
+	//condition to exit if head is not true
+	sim = compare_paths(path->move, head->move);
+	return (sim);
+}
+
+void	delete_sim(t_room *pass, int t_len, int	delete)
+{
+	t_path *head;
+	t_path *prev_head;
+	t_path	*temp;
+	t_index	*home;
+
+	head = pass->head;
+	prev_head = NULL;
+	while (pass->head)
+	{
+		home = pass->head->move;
+		while (pass->head->move)
+		{
+			if (pass-head->move->index == delete && pass->head->len > t_len)
+			{
+				temp = pass->head->next;
+				free(pass->head); //and everything inside
+				if (!prev_head)
+					pass->head = temp;
+				else
+					prev_head->next = temp;
+			}	
+			pass->head->move = pass->head->move->next;
+		}
+		pass->head->move = home;
+		prev_head = pass->head;
+		pass->head = pass->head->next;
+	}
+	pass->head = head;
+}
 
 void	absolute_path(t_room * pass)
 {
 	t_index	*tmp_indx;
 	t_path 	*tmp_path;
 	int		j;
+	int		ret;
 
 	j = 1;
+	ret = -1;
+	tmp_path = pass->head;
 	while ( j <= length of longest path)
 	{
-		while (pass->path)
+		while (tmp_path->path)
 		{
-			if (pass->path->len == j)
+			if (j == tmp_path->len)
+				ret = keep_and_erase(tmp_path->nbr, tmp_path, pass->head);
+			if (ret != -1)
 			{
-				if (!pass->path->next || path->next->len > j)
-					//keep the path we're currently on
-					//break loop
-				tmp_indx = path->move;
-				tmp_path = path;
-				tmp_path = tmp_path->next;
-				while (tmp_path->len == j)
-				{
-					if (tmp_path->move->index == tmp_index->index)
-					{
-
-					}
-				}
+				delete_sim(pass, j, ret);
+				ret = -1;
 			}
+			tmp_path = tmp_path->next;
 		}
+		tmp_path = pass->head;
 		++J;
 	}
 }
