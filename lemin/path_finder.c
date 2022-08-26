00024bc494641a608bdf05ab6d9d63212824f5d0
Jefferson Davis <davisjd.jefferson23@gmail.com>
2022-08-26 17:47:04 +0300

    modified lock_path function

--- lemin/path_finder.c
+++ lemin/path_finder.c
@@ -221,30 +221,74 @@ static void	print_output(t_room *pass)
 static void	lock_path(t_room *pass, int indx)
 {
 	int	hold;
+	int	for_now;
+	int	value;
+	int	next;
 
+	next = 0;
+	hold = 0;
 	pass->info[NEXT][indx] = pass->end; 
-	ft_printf("FIRST INDX %s\n", pass->rooms[indx]);
 	while (indx != 0)
 	{
-		if (pass->info[PATH][indx] == 3)
+		for_now = indx;
+		value = pass->info[PATH][indx];
+		//pass->info[PATH][indx] = 2;
+		if (!next)
+			indx = pass->info[PREV][indx];
+		else
+			indx = next;
+		next = 0;
+		/*if (pass->info[PATH][indx] == 1)
 		{
-			hold = pass->info[NEXT][indx];
-			pass->info[NEXT][indx] = pass->info[PREV][indx];
-			indx = hold;
-			while (pass->info[JUMP][indx] != 0)
-			{
-				if (pass->info[PATH][indx] == 3)
-					pass->info[PATH][indx] = 2;
-				indx = pass->info[PREV][indx];
-				//pass->info[NEXT][indx];
-				//if (pass->info[PREV][indx] != pass->info[NEXT][indx])
-			}
-			if (pass->info[JUMP][indx] != 0)
-				pass->info[PREV][indx] = pass->info[JUMP][indx];
+			pass->info[NEXT][indx] = for_now;
+			hold = 0;
 		}
-		pass->info[PATH][indx] = 2;
-		indx = pass->info[PREV][indx];
-	}
+		else */if ((pass->info[PATH][indx] == 3 && value == 1) || (pass->info[PATH][indx] == 1 && value == 3) || pass->info[PATH][indx] == 1)
+		{
+			hold = 0;
+			if (value == 1 && pass->info[PATH][indx] != 1)
+			{
+				next = pass->info[NEXT][indx];
+				hold = 1;
+			}
+			pass->info[NEXT][indx] = for_now;
+		}
+		else if (pass->info[PATH][indx] == 3 && value == 3 && pass->info[JUMP][indx] && !hold)
+		{
+			if (!hold)
+			{
+				pass->info[PREV][indx] = pass->info[JUMP][indx];
+				hold = 1;
+			}
+			else
+			{
+				next = pass->info[NEXT][indx];
+				pass->info[NEXT][indx] = for_now;
+				hold = 0;
+			}
+		}
+		/*else if (pass->info[PATH][indx] == 3 && value == 3 && pass->info[JUMP][indx] && hold)
+		{
+			next = pass->info[NEXT][indx];
+			pass->info[NEXT][indx] = for_now;
+			hold = 0;
+		}*/
+		else if (pass->info[PATH][indx] == 3 && value == 3 && !pass->info[JUMP][indx])
+		{
+			next = pass->info[NEXT][indx];
+			if (!hold)
+			{
+				pass->info[PREV][indx] = 0;
+				pass->info[NEXT][indx] = 0;
+				pass->info[PATH][indx] = 0;
+			}
+			else
+			{
+				next = pass->info[NEXT][indx];
+				pass->info[NEXT][indx] = for_now;
+			}
+			hold = 0;
+		}
 
 }
 
