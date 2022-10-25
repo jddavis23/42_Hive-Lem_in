1ad579116e918fa6e31a47cff823dd92c2e8312e
Jefferson Davis <davisjd.jefferson23@gmail.com>
2022-10-25 12:28:09 +0300

    initialising linkd variable

--- lemin/parse_count.c
+++ lemin/parse_count.c
@@ -6,81 +6,134 @@
 /*   By: jdavis <jdavis@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/11 13:20:20 by jdavis            #+#    #+#             */
-/*   Updated: 2022/09/14 17:50:10 by jdavis           ###   ########.fr       */
+/*   Updated: 2022/10/25 12:27:12 by jdavis           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/lemin.h"
+#include <stdio.h>
 
-static int	found_or_not(t_room *pass, int r, char *temp, char *input)
+// static int	found_or_not(t_room *pass, int r, char *temp, char *input)
+// {
+// 	int	str_len;
+
+// 	if (temp && input[0] != '#')
+// 	{
+// 		str_len = ft_strlen(pass->rooms[r]);
+// 		if (((temp[-1] == '\n' && temp[str_len] == '-') || \
+// 			(temp[-1] == '-' && temp[str_len] == '\n')))
+// 		{
+// 			if (is_dash(&temp[str_len]) >= 1 && (temp[-1] == '\n' && \
+// 				temp[str_len] == '-'))
+// 				return (newline_minus(pass, r, temp, input));
+// 			else if (dash_in_section(input, temp) >= 1 && \
+// 				(temp[-1] == '-' && temp[str_len] == '\n'))
+// 				return (minus_newline(pass, r, input, temp));
+// 		}
+// 	}
+// 	return (0);
+// }
+
+static int	count_in_helper(t_room *pass, char *input)
 {
-	int	str_len;
+	if (!ft_strncmp(&input[0], "##start\n", 8) || \
+		!ft_strncmp(&input[0], "##end\n", 6))
+		return (free_connect(pass));
+	return (1);
+}
 
-	if (temp && input[0] != '#')
+static int	initialise_links(t_room *pass)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (i < pass->total)
 	{
-		str_len = ft_strlen(pass->rooms[r]);
-		if (((temp[-1] == '\n' && temp[str_len] == '-') || \
-			(temp[-1] == '-' && temp[str_len] == '\n')))
+		pass->links[i] = (int *) malloc((pass->con_arr[i]->count - 1) * sizeof(int));
+		if (!pass->links[i])
+			return (-1);//return (error_free(pass, NULL, 0, FALSE));//double check freeing
+		j = 0;
+		while (j < (pass->con_arr[i]->count - 1))
 		{
-			if (is_dash(&temp[str_len]) >= 1 && (temp[-1] == '\n' && \
-				temp[str_len] == '-'))
-				return (newline_minus(pass, r, temp, input));
-			else if (dash_in_section(input, temp) >= 1 && \
-				(temp[-1] == '-' && temp[str_len] == '\n'))
-				return (minus_newline(pass, r, input, temp));
+			pass->links[i][j] = pass->con_arr[i]->head->current_room;
+			pass->con_arr[i]->head = pass->con_arr[i]->head->next;
+			++j;
 		}
+		++i;
 	}
 	return (0);
 }
 
-static int	count_in_helper(t_room *pass, char *input, int r, char *temp)
-{
-	int	stop;
-
-	if (temp && input[0] != '#')
-	{
-		stop = ft_strlen_stop(&temp[1], '\n');
-		while (ft_strnstr(&temp[1], pass->rooms[r], stop))
-		{
-			temp = ft_strnstr(&temp[1], pass->rooms[r], stop);
-			if (temp[-1] == '-' && temp[ft_strlen(pass->rooms[r])] == '\n')
-			{
-				if (minus_newline(pass, r, &input[0], temp) == -1)
-					return (-1);
-			}
-		}
-	}
-	else
-	{
-		if (!ft_strncmp(&input[0], "##start\n", 8) || \
-			!ft_strncmp(&input[0], "##end\n", 6))
-			return (free_connect(pass));
-	}
-	return (1);
-}
-
-int	count_in(int r, char *input, t_room *pass)
+int	count_in(char *input, t_room *pass)
 {
 	int		i;
-	char	*temp;
 	int		j;
+	int		stop;
+	int		look;
+	int		end;
 
 	i = 0;
-	j = 0;
-	pass->con_prev = pass->tmp_con->count;
 	while (input[i] != '\0')
 	{
-		temp = ft_strnstr(&input[i], pass->rooms[r], \
-			ft_strlen_stop(&input[i], '\n'));
-		if (found_or_not(pass, r, temp, &input[i]) == -1)
-			return (-1);
-		if (count_in_helper(pass, &input[i], r, temp) == -1)
-			return (-1);
-		if (wrong_line(pass, r, &j, &input[i]) == ERROR)
-			return (-1);
+		stop = ft_strlen_stop(&input[i], '\n');
+		j = 0;
+		j += dash_in_section(&input[i + j], &input[i + stop]);
+		end = 0;
+		while (input[i] != '#' && input[i + j] != '\n')
+		{
+			look = 0;
+			while (look < pass->total)
+			{
+				if (!ft_strncmp(&input[i], pass->rooms[look], j))
+				{
+					if (newline_minus(pass, look, &input[i], &input[i]) == 1)
+					{
+						end = 2;
+						break ;
+					}
+				}
+				look++;
+			}
+			if (end == 2)
+				break ;
+			j += dash_in_section(&input[i + j], &input[i + stop]);
+			if (input[i + j] == '\n') //this isnt workin gto spot an error
+			 	return (-1);
+		}
+		if (count_in_helper(pass, &input[i]) == -1)
+		{
+		 	return (-1);
+		}
 		while (input[i] != '\n')
 			++i;
 		++i;
 	}
+	if (initialise_links(pass))
+		return (-1);
+	i = 0;
+	while (i < pass->total)
+	{
+		j = 0;
+		while (j < pass->con_arr[i]->count - 1)
+			ft_printf("%s   ", pass->rooms[pass->links[i][j++]]);
+		ft_printf("\n");
+		++i;
+	}
+	exit (0);
+	// i = 0;
+	// while (i < pass->total)
+	// {
+	// 	while (pass->con_arr[i]->head->current_room != -1)
+	// 	{
+	// 		ft_printf("%s   ", pass->rooms[pass->con_arr[i]->head->current_room]);
+	// 		pass->con_arr[i]->head = pass->con_arr[i]->head->next;
+	// 	}
+	// 	ft_printf("\n");
+	// 	if (i == 1)
+	// 		exit (0);
+	// 	++i;
+
+	// }
 	return (0);
 }
